a4db7f6d6c40345240657d1d399c0e93903df00e
khouloudcherrat <khouloudcherrat@gmail.com>
2024-11-20 09:51:00 -0500

    TDD : Add test case to update a product & function to make test pass

--- service/routes.py
+++ service/routes.py
@@ -129,9 +129,25 @@ def get_products(product_id):
 # U P D A T E   A   P R O D U C T
 ######################################################################
 
-#
-# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
-#
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_products(product_id):
+    """
+    Update an Product
+    This endpoint will update a Product based on the body that is posted
+    """
+    app.logger.info("Request to Update a product with id [%s]", product_id)
+    check_content_type("application/json")
+
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+
+    app.logger.info("Updating product: %s", product.name)
+    
+    product.deserialize(request.get_json())
+    product.update()
+
+    return product.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # D E L E T E   A   P R O D U C T
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -182,6 +182,33 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         self.assertIn("was not found", data["message"])
 
+    # ----------------------------------------------------------
+    # TEST UPDATE A PRODUCT
+    # ----------------------------------------------------------
+    def test_update_product(self):
+        """It should update an exsiting Product"""
+        # Create product to update
+        test_product = ProductFactory()
+        response = self.client.post(f"{BASE_URL}", json = test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        # Update the product
+        new_product = response.get_json()
+        new_product["description"] = "unknown"
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json = new_product)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product["description"], "unknown")
+    
+    def test_update_product_not_found(self):
+        """It should not update a Product thats not found"""
+        test_product = ProductFactory()
+        test_product.id = 0
+        response = self.client.put(f"{BASE_URL}/{test_product.id}", json = test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        data = response.get_json()
+        self.assertIn("was not found", data["message"])
+
     ######################################################################
     # Utility functions
     ######################################################################
